26579468b0d406259a9c07ca30dc22b14528b68a
Tobias Mende <i@tobias-men.de>
2018-10-27 11:50:20 +0200

    Initial Demo Application

---
+++ README.md
@@ -0,0 +1,2 @@
+# MetaNook2018 Ansible Demo Application
+This repository contains a small NodeJS application for demonstration purposes in the Ansible session.
---
+++ app.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env nodejs
+const http = require('http');
+const config = require('./config')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Hello World from ${config.hostname}\n`);
+});
+
+server.listen(config.port, config.hostname, () => {
+  console.log(`Server running at http://${config.hostname}:${config.port}/`);
+});
